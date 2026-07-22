841e48e000fe1f3ba015600526a9531bf2439c4d
Parth Singh <parthsingh1866@gmail.com>
2026-07-23 04:21:36 +0530

    Error Handeling and Response Models Learned and applied

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Path
-from typing import Optional
+from fastapi import FastAPI, HTTPException
 from app.models import Product # Importing our base model
+from app.models import Item, UserIn, UserBase, UserOut
 
 app = FastAPI(
     title="Sales Analytics API",
@@ -33,20 +33,26 @@ def health():
 
 
 @app.get("/get-product/{product_id}")
-def get_product_details(product_id: int = Path(description="ID of the product you want to view")):
+def get_product_details(product_id: int):
     if product_id in products:
         return products[product_id]
-    return {"Message":"ID not found"}
+    raise HTTPException(
+        status_code=404,
+        detail="Product Not found."
+    )
 
 @app.get("/get-product-by-name/{name}")
-def get_product_by_name(name: str = Path(description="name of the product you want to view")):
+def get_product_by_name(name: str):
     for product in products.values():
         if product["name"] == name:
             return product
-    return {"Message":"Product Not Found"}
+    raise HTTPException(
+        status_code=404,
+        detail="Product Not found."
+    )
 
 @app.get("/products-by-id-and-category/")
-def get_product(product_id: int, cat: Optional[str] = None):
+def get_product(product_id: int, cat: str | None = None):
     if product_id in products:
         product = products[product_id]
         if cat is None: 
@@ -54,13 +60,28 @@ def get_product(product_id: int, cat: Optional[str] = None):
         else:
             if product['category'] == cat:
                 return product['name']
-    return {"Data": "Invalid ID or Category"}     
+    raise HTTPException(
+        status_code=400,
+        detail="Invalid ID or category."
+    )    
 
 
-@app.post("/create-new-product/{product_id}")
+@app.post("/create-new-product/{product_id}", response_model=Product, status_code=201)
 def create_new_product(product_id: int, productDetail: Product):
     if product_id in products:
-        return {"Error": "Product already Exists"}
-
+        # return {"Error": "Product already Exists"}
+        raise HTTPException(
+            status_code=409,
+            detail="Product already Exists."
+        )
     products[product_id] = productDetail.model_dump()
     return products[product_id]
+
+
+@app.post("/items/", response_model = Item)
+def create_item(item: Item):
+    return item 
+
+@app.post("/user/", response_model=UserOut)
+def add_user(user: UserIn):
+    return user
--- app/models.py
+++ app/models.py
@@ -1,7 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class Product(BaseModel):
     name: str
     price: float
     stock: int
     category: str
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
+    passwd: str
+
+class UserOut(UserBase):
+    pass
