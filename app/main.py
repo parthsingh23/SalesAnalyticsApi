4f51fa6f3a92b746ca6f10523def28b962077c0a
Parth Singh <parthsingh1866@gmail.com>
2026-07-23 03:03:01 +0530

    Completed Week1 Monday and Tuesday. KPIs for csv and list comprehension completed. POST endpoint also learned and practiced.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from typing import Optional
-from pydantic import BaseModel
+from app.models import Product # Importing our base model
 
 app = FastAPI(
     title="Sales Analytics API",
@@ -8,6 +8,14 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
+products = {
+    1: {"name": "Mouse", "price": 25.99, "stock": 10,  "category": "Electronics"},
+    2: {"name": "Shoes", "price": 79.99, "stock": 20, "category": "Footwear"},
+    3: {"name": "Mug", "price": 12.50, "stock": 45, "category": "Kitchen"},
+    4: {"name": "Notebook", "price": 5.99, "stock": 35, "category": "Stationery"},
+    5: {"name": "Speaker", "price": 49.99, "stock": 80, "category": "Electronics"},
+}
 
 @app.get("/")
 def root():
@@ -24,30 +32,20 @@ def health():
     return {"status": "healthy"}
 
 
-products = {
-    1: {"name": "Mouse", "price": 25.99, "category": "Electronics"},
-    2: {"name": "Shoes", "price": 79.99, "category": "Footwear"},
-    3: {"name": "Mug", "price": 12.50, "category": "Kitchen"},
-    4: {"name": "Notebook", "price": 5.99, "category": "Stationery"},
-    5: {"name": "Speaker", "price": 49.99, "category": "Electronics"},
-}
+@app.get("/get-product/{product_id}")
+def get_product_details(product_id: int = Path(description="ID of the product you want to view")):
+    if product_id in products:
+        return products[product_id]
+    return {"Message":"ID not found"}
 
+@app.get("/get-product-by-name/{name}")
+def get_product_by_name(name: str = Path(description="name of the product you want to view")):
+    for product in products.values():
+        if product["name"] == name:
+            return product
+    return {"Message":"Product Not Found"}
 
-@app.get("/get-product-details")
-def get_product_name(name: str):
-    for product_id in products:
-        if products[product_id]["name"] == name:
-            return products[product_id]
-    return {"Data": "Product Not Found"}
-
-@app.get("/get-product-category")
-def product_category(name: str):
-    for product_id in products:
-        if products[product_id]["name"] == name:
-            return products[product_id]["category"]
-    return {"Data": "Product Not Found"}
-
-@app.get("/products-by-id-and-category")
+@app.get("/products-by-id-and-category/")
 def get_product(product_id: int, cat: Optional[str] = None):
     if product_id in products:
         product = products[product_id]
@@ -59,8 +57,10 @@ def get_product(product_id: int, cat: Optional[str] = None):
     return {"Data": "Invalid ID or Category"}     
 
 
-@app.get("/sales")
-def get_sales(year: int, month: int, category: str = "All"):
-    return {
-        "year": year, "month": month, "category": category
-    }
+@app.post("/create-new-product/{product_id}")
+def create_new_product(product_id: int, productDetail: Product):
+    if product_id in products:
+        return {"Error": "Product already Exists"}
+
+    products[product_id] = productDetail.model_dump()
+    return products[product_id]
